f59813a5219e1c34295e6db85a2b894e6aabc420
Amal <almaabadyamal@gmail.com>
2022-11-14 14:37:46 +0300

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Eman on 20/04/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
